edb1a21e81260400dd8cfaa906a045501ad8d692
Rasel <mdraselhossain2541@gmail.com>
2020-09-26 22:59:39 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBcuETuxyZfKQ4QNk2JKc-HeuT3SiXjh_U",
+  authDomain: "assignment-9-36098.firebaseapp.com",
+  databaseURL: "https://assignment-9-36098.firebaseio.com",
+  projectId: "assignment-9-36098",
+  storageBucket: "assignment-9-36098.appspot.com",
+  messagingSenderId: "343651122428",
+  appId: "1:343651122428:web:4ba95d292b207fa3b76b79"
+};
+export default firebaseConfig;
